1fe184bc5c236a8dffb60b25a437393f62b5fee9
eludolf-pucpr <81109804+eludolf-pucpr@users.noreply.github.com>
2022-06-09 00:20:42 -0300

    sa repo

---
+++ usuario_programa_especialistas.sql
@@ -0,0 +1,5 @@
+# Privileges for `usuario_programaespecialistas`@`%`
+
+GRANT ALL PRIVILEGES ON *.* TO `usuario_programaespecialistas`@`%` IDENTIFIED BY PASSWORD '*B625F26BA7CF894AEC1F0F6C32488512232A6EEF' WITH GRANT OPTION;
+
+GRANT ALL PRIVILEGES ON `programa\_especialistas`.* TO `usuario_programaespecialistas`@`%` WITH GRANT OPTION;
